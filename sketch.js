7c7662f0af7dd81cea73d7f92b18a77ff3903a0f
Hasika2008 <91638771+Hasika2008@users.noreply.github.com>
2021-10-14 17:25:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  for(var i=0; i<marks.length; i++){
+    if(marks[i]=== 45){
+      console.log("45 is present in the array "+i)
+    }
+    else(
+      console.log("45 is not present in the array ")
+    )
+  }
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
